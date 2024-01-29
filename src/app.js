a5f31dca0d1e72a56ae55d43c7e8d98b92c6d554
Eugene <josumkin@yahoo.com>
2024-01-29 20:14:16 +0400

    feat: exit app added

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import readline from 'readline';
+import os from 'os';
+
+const homeDir = os.homedir();
+const userArgs = process.argv;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+rl.setPrompt('> ');
+rl.prompt();
+
+let username = '';
+
+userArgs.forEach((val) => {
+    if (val.slice(2, 10) === 'username') {
+        username = val.split('=')[1];
+    }
+});
+
+if (username !== '') {
+    console.log(`Welcome to the File Manager, ${username}!`);
+    console.log(`\x1b[32mYou are currently in ${homeDir}\x1b[0m`);
+} else {
+    rl.question('\x1b[32mWhat is your name? \x1b[0m', (name) => {
+        username = name;
+        console.log(`Welcome to the File Manager, ${username}!`);
+        console.log(`\x1b[32mYou are currently in ${homeDir}\x1b[0m`);
+      });
+}
+
+rl.on('line', (input) => {
+    switch (input.trim()) {
+      case 'os --EOL':
+        console.log(`Operating system information: ${os.platform()} ${os.release()}`);
+        break;
+      case 'os --cpus':
+        console.log(`Processor information: ${JSON.stringify(os.cpus())}`);
+        break;
+      case '.exit':
+        console.log('\x1b[31m%s\x1b[0m', 'Goodbye!');
+        rl.close();
+        break;
+      default:
+        console.log(`Invalid input: ${input.trim()}`);
+        break;
+    }
+  
+    rl.prompt();
+  }).on('close', () => {
+    console.log('Exiting...');
+    process.exit(0);
+  });
